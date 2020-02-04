9a7f900869f0ff14469e482cde3d8cac255159a7
Amy Franz <amy.franz@gmail.com>
2020-02-04 16:58:50 +0000

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,17 @@
-module memorable
-    def self.reset_all
-      self.all.clear
-    end
+module Memorable
+    module ClassMethods
+        def reset_all
+            self.all.clear
+        end
     
-    def self.count
-      self.all.count
+        def count
+            self.all.count
+        end
+    end
+
+    module InstanceMethods
+        def initialize
+            self.class.all << self
+        end
     end
 end
